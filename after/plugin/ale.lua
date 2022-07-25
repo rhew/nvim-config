498601d994638096a2282bff3928836d8c6388db
James Rhew <james@rhew.org>
2022-07-24 20:13:02 -0400

    golangci-lint support

--- after/plugin/ale.lua
+++ after/plugin/ale.lua
@@ -2,3 +2,7 @@ local map = require("utils").map
 map ("n", "<leader>at", ":ALEToggle<CR>")
 vim.g.ale_yaml_yamllint_options = '-d "{extends: default, rules: {line-length: {max: 100}}}"'
 vim.b.ale_fix_on_save = 0
+vim.cmd [[
+  let g:ale_linters = {'go': ['gofmt', 'golint', 'gopls', 'govet', 'golangci-lint'] }
+]]
+vim.g.ale_go_golangci_lint_options = ''
